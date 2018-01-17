03202f9f489c994c93d3dfd7811ba0db926a05d5
wbcz <bo.wang09@ele.me>
2018-01-17 13:51:43 +0800

    Create App.vue

---
+++ src/client/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div @click="trigger">
+        <topics ref="topics"></topics>
+    </div>
+</template>
+<script>
+    import topics from './components/topics.vue'
+
+    export default {
+        data(){
+            return {}
+        },
+        beforeCreate() {
+            console.log('App beforeCreate')
+        },
+        created() {
+            console.log('App created')
+        },
+        beforeMount() {
+            console.log('App beforeMount')
+        },
+        mounted() {
+            console.log('App mounted')
+        },
+        methods: {
+            trigger() {
+              console.log('触发点击事件')
+            }
+        },
+        components: {
+            topics: topics,
+        }
+    }
+</script>
